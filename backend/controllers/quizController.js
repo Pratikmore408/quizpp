9b6f428ffab957ce6058d76c68b645cafee5a94a
Pratik More <pratikmore408@gmail.com>
2024-08-12 15:38:36 +0530

    final build added

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -3,8 +3,6 @@ import Question from "../models/Question.js";
 export const getQuestions = async (req, res) => {
   try {
     const questions = await Question.find();
-    // console.log(questions);
-
     res.json(questions);
   } catch (error) {
     res.status(500).json({ message: error.message });
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import quizRoutes from "./routes/quizRoutes.js";
@@ -11,9 +12,18 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+const __dirname = path.resolve();
 
 app.use("/api/quiz", quizRoutes);
 app.use("/api/tags", tagRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.use(errorHandler);
 
